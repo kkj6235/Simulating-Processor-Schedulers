da4e89355d6e13496c87b023f4dd838deaceb824
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2024-04-22 15:38:11 +0900

    feat : RR scheduler

--- pa2.c
+++ pa2.c
@@ -24,6 +24,7 @@
  * The process which is currently running
  */
 #include "process.h"
+
 extern struct process *current;
 
 /**
@@ -35,6 +36,7 @@ extern struct list_head readyqueue;
  * Resources in the system.
  */
 #include "resource.h"
+
 extern struct resource resources[NR_RESOURCES];
 
 /**
@@ -56,30 +58,29 @@ extern bool quiet;
  *   The current implementation serves the resource in the requesting order
  *   without considering the priority. See the comments in sched.h
  ***********************************************************************/
-static bool fcfs_acquire(int resource_id)
-{
-	struct resource *r = resources + resource_id;
+static bool fcfs_acquire(int resource_id) {
+    struct resource *r = resources + resource_id;
 
-	if (!r->owner) {
-		/* This resource is not owned by any one. Take it! */
-		r->owner = current;
-		return true;
-	}
+    if (!r->owner) {
+        /* This resource is not owned by any one. Take it! */
+        r->owner = current;
+        return true;
+    }
 
-	/* OK, this resource is taken by @r->owner. */
+    /* OK, this resource is taken by @r->owner. */
 
-	/* Update the current process state */
-	current->status = PROCESS_BLOCKED;
+    /* Update the current process state */
+    current->status = PROCESS_BLOCKED;
 
-	/* And append current to waitqueue */
-	list_add_tail(&current->list, &r->waitqueue);
+    /* And append current to waitqueue */
+    list_add_tail(&current->list, &r->waitqueue);
 
-	/**
-	 * And return false to indicate the resource is not available.
-	 * The scheduler framework will soon call schedule() function to
-	 * schedule out current and to pick the next process to run.
-	 */
-	return false;
+    /**
+     * And return false to indicate the resource is not available.
+     * The scheduler framework will soon call schedule() function to
+     * schedule out current and to pick the next process to run.
+     */
+    return false;
 }
 
 /***********************************************************************
@@ -91,42 +92,41 @@ static bool fcfs_acquire(int resource_id)
  *   The current implementation serves the resource in the requesting order
  *   without considering the priority. See the comments in sched.h
  ***********************************************************************/
-static void fcfs_release(int resource_id)
-{
-	struct resource *r = resources + resource_id;
+static void fcfs_release(int resource_id) {
+    struct resource *r = resources + resource_id;
 
-	/* Ensure that the owner process is releasing the resource */
-	assert(r->owner == current);
+    /* Ensure that the owner process is releasing the resource */
+    assert(r->owner == current);
 
-	/* Un-own this resource */
-	r->owner = NULL;
+    /* Un-own this resource */
+    r->owner = NULL;
 
-	/* Let's wake up ONE waiter (if exists) that came first */
-	if (!list_empty(&r->waitqueue)) {
-		struct process *waiter = list_first_entry(&r->waitqueue, struct process, list);
+    /* Let's wake up ONE waiter (if exists) that came first */
+    if (!list_empty(&r->waitqueue)) {
+        struct process *waiter = list_first_entry(&r->waitqueue, struct process, list);
 
-		/**
-		 * Ensure the waiter is in the wait status
-		 */
-		assert(waiter->status == PROCESS_BLOCKED);
+        /**
+         * Ensure the waiter is in the wait status
+         */
+        assert(waiter->status == PROCESS_BLOCKED);
 
-		/**
-		 * Take out the waiter from the waiting queue. Note we use
-		 * list_del_init() over list_del() to maintain the list head tidy
-		 * (otherwise, the framework will complain on the list head
-		 * when the process exits).
-		 */
-		list_del_init(&waiter->list);
+        /**
+         * Take out the waiter from the waiting queue. Note we use
+         * list_del_init() over list_del() to maintain the list head tidy
+         * (otherwise, the framework will complain on the list head
+         * when the process exits).
+         */
+        list_del_init(&waiter->list);
 
-		/* Update the process status */
-		waiter->status = PROCESS_READY;
+        /* Update the process status */
+        waiter->status = PROCESS_READY;
 
-		/**
-		 * Put the waiter process into ready queue. The framework will
-		 * do the rest.
-		 */
-		list_add_tail(&waiter->list, &readyqueue);
-	}
+        /**
+         * Put the waiter process into ready queue. The framework will
+         * do the rest.
+         */
+        list_add_tail(&waiter->list, &readyqueue);
+    }
 }
 
 #include "sched.h"
@@ -134,74 +134,70 @@ static void fcfs_release(int resource_id)
 /***********************************************************************
  * FCFS scheduler
  ***********************************************************************/
-static int fcfs_initialize(void)
-{
-	return 0;
+static int fcfs_initialize(void) {
+    return 0;
 }
 
-static void fcfs_finalize(void)
-{
+static void fcfs_finalize(void) {
 }
 
-static struct process *fcfs_schedule(void)
-{
-	struct process *next = NULL;
+static struct process *fcfs_schedule(void) {
+    struct process *next = NULL;
 
-	/* You may inspect the situation by calling dump_status() at any time */
-	// dump_status();
+    /* You may inspect the situation by calling dump_status() at any time */
+    // dump_status();
 
-	/**
-	 * When there was no process to run in the previous tick (so does
-	 * in the very beginning of the simulation), there will be
-	 * no @current process. In this case, pick the next without examining
-	 * the current process. Also, the current process can be blocked
-	 * while acquiring a resource. In this case just pick the next as well.
-	 */
-	if (!current || current->status == PROCESS_BLOCKED) {
-		goto pick_next;
-	}
+    /**
+     * When there was no process to run in the previous tick (so does
+     * in the very beginning of the simulation), there will be
+     * no @current process. In this case, pick the next without examining
+     * the current process. Also, the current process can be blocked
+     * while acquiring a resource. In this case just pick the next as well.
+     */
+    if (!current || current->status == PROCESS_BLOCKED) {
+        goto pick_next;
+    }
 
-	/* The current process has remaining lifetime. Schedule it again */
-	if (current->age < current->lifespan) {
-		return current;
-	}
+    /* The current process has remaining lifetime. Schedule it again */
+    if (current->age < current->lifespan) {
+        return current;
+    }
 
-pick_next:
-	/* Let's pick a new process to run next */
+    pick_next:
+    /* Let's pick a new process to run next */
 
-	if (!list_empty(&readyqueue)) {
-		/**
-		 * If the ready queue is not empty, pick the first process
-		 * in the ready queue
-		 */
-		next = list_first_entry(&readyqueue, struct process, list);
+    if (!list_empty(&readyqueue)) {
+        /**
+         * If the ready queue is not empty, pick the first process
+         * in the ready queue
+         */
+        next = list_first_entry(&readyqueue, struct process, list);
 
-		/**
-		 * Detach the process from the ready queue. Note that we use
-		 * list_del_init() over list_del() to maintain the list head tidy.
-		 * Otherwise, the framework will complain (assert) on process exit.
-		 */
-		list_del_init(&next->list);
-	}
+        /**
+         * Detach the process from the ready queue. Note that we use
+         * list_del_init() over list_del() to maintain the list head tidy.
+         * Otherwise, the framework will complain (assert) on process exit.
+         */
+        list_del_init(&next->list);
+    }
 
-	/* Return the process to run next */
-	return next;
+    /* Return the process to run next */
+    return next;
 }
 
 struct scheduler fcfs_scheduler = {
-	.name = "FCFS",
-	.acquire = fcfs_acquire,
-	.release = fcfs_release,
-	.initialize = fcfs_initialize,
-	.finalize = fcfs_finalize,
-	.schedule = fcfs_schedule,
+        .name = "FCFS",
+        .acquire = fcfs_acquire,
+        .release = fcfs_release,
+        .initialize = fcfs_initialize,
+        .finalize = fcfs_finalize,
+        .schedule = fcfs_schedule,
 };
 
 /***********************************************************************
  * SJF scheduler
  ***********************************************************************/
-static struct process *sjf_schedule(void)
-{
+static struct process *sjf_schedule(void) {
     struct process *next = NULL;
 
 
@@ -220,12 +216,11 @@ static struct process *sjf_schedule(void)
 
         struct process *p = NULL;
 
-        list_for_each_entry(p,&readyqueue,list) {
-            if(!next){
+        list_for_each_entry(p, &readyqueue, list) {
+            if (!next) {
                 next = p;
-            }
-            else{
-                if(p->lifespan<next->lifespan){
+            } else {
+                if (p->lifespan < next->lifespan) {
                     next = p;
                 }
             }
@@ -238,10 +233,10 @@ static struct process *sjf_schedule(void)
 }
 
 struct scheduler sjf_scheduler = {
-	.name = "Shortest-Job First",
-	.acquire = fcfs_acquire,	/* Use the default FCFS acquire() */
-	.release = fcfs_release,	/* Use the default FCFS release() */
-	.schedule = sjf_schedule,			/* TODO: Assign your schedule function
+        .name = "Shortest-Job First",
+        .acquire = fcfs_acquire,    /* Use the default FCFS acquire() */
+        .release = fcfs_release,    /* Use the default FCFS release() */
+        .schedule = sjf_schedule,            /* TODO: Assign your schedule function
 								   to this function pointer to activate
 								   SJF in the simulation system */
 };
@@ -250,51 +245,48 @@ struct scheduler sjf_scheduler = {
  * STCF scheduler
  ***********************************************************************/
 
-static struct process *stcf_schedule(void)
-{
+static struct process *stcf_schedule(void) {
     struct process *next = NULL;
 
     if (!list_empty(&readyqueue)) {
 
         struct process *p = NULL;
 
-        list_for_each_entry(p,&readyqueue,list) {
-            if(!next){
+        list_for_each_entry(p, &readyqueue, list) {
+            if (!next) {
                 next = p;
-            }
-            else{
-                if((p->lifespan-p->age)<(next->lifespan-next->age)){
+            } else {
+                if ((p->lifespan - p->age) < (next->lifespan - next->age)) {
                     next = p;
                 }
             }
         }
-        if(!current){
+        if (!current) {
             list_del_init(&next->list);
             return next;
         }
-        if(current->lifespan!=current->age){
-            if((current->lifespan-current->age)<(next->lifespan-next->age)){
+        if (current->lifespan != current->age) {
+            if ((current->lifespan - current->age) < (next->lifespan - next->age)) {
                 next = current;
-            }
-            else{
-                list_add_tail(&current->list,&readyqueue);
+            } else {
+                list_add_tail(&current->list, &readyqueue);
             }
         }
         list_del_init(&next->list);
 
-    }
-    else{
+    } else {
         if (current->age < current->lifespan) {
             return current;
         }
     }
     return next;
 }
+
 struct scheduler stcf_scheduler = {
-	.name = "Shortest Time-to-Complete First",
-	.acquire = fcfs_acquire, /* Use the default FCFS acquire() */
-	.release = fcfs_release, /* Use the default FCFS release() */
-    .schedule = stcf_schedule,
+        .name = "Shortest Time-to-Complete First",
+        .acquire = fcfs_acquire, /* Use the default FCFS acquire() */
+        .release = fcfs_release, /* Use the default FCFS release() */
+        .schedule = stcf_schedule,
         /* You need to check the newly created processes to implement STCF.
          * Have a look at @forked() callback.
          */
@@ -304,52 +296,78 @@ struct scheduler stcf_scheduler = {
 /***********************************************************************
  * Round-robin scheduler
  ***********************************************************************/
-struct scheduler rr_scheduler = {
-	.name = "Round-Robin",
-	.acquire = fcfs_acquire, /* Use the default FCFS acquire() */
-	.release = fcfs_release, /* Use the default FCFS release() */
 
-	/* Obviously, ... */
+static struct process *rr_schedule(void) {
+    struct process *next = NULL;
+
+    if(!list_empty(&readyqueue)){
+        next = list_first_entry(&readyqueue, struct process, list);
+        list_del_init(&next->list);
+
+        if (current) {
+            if (current->lifespan != current->age) {
+                list_add_tail(&current->list, &readyqueue);
+            }
+        }
+    }
+    else{
+        if(current->age<current->lifespan){
+            return current;
+        }
+    }
+
+
+    return next;
+}
+
+
+struct scheduler rr_scheduler = {
+        .name = "Round-Robin",
+        .acquire = fcfs_acquire, /* Use the default FCFS acquire() */
+        .release = fcfs_release, /* Use the default FCFS release() */
+        .schedule = rr_schedule,
+
+        /* Obviously, ... */
 };
 
 /***********************************************************************
  * Priority scheduler
  ***********************************************************************/
 struct scheduler prio_scheduler = {
-	.name = "Priority",
-	/**
-	 * Implement your own acqure/release function to make the priority
-	 * scheduler correct.
-	 */
-	/* Implement your own prio_schedule() and attach it here */
+        .name = "Priority",
+        /**
+         * Implement your own acqure/release function to make the priority
+         * scheduler correct.
+         */
+        /* Implement your own prio_schedule() and attach it here */
 };
 
 /***********************************************************************
  * Priority scheduler with aging
  ***********************************************************************/
 struct scheduler pa_scheduler = {
-	.name = "Priority + aging",
-	/**
-	 * Ditto
-	 */
+        .name = "Priority + aging",
+        /**
+         * Ditto
+         */
 };
 
 /***********************************************************************
  * Priority scheduler with priority ceiling protocol
  ***********************************************************************/
 struct scheduler pcp_scheduler = {
-	.name = "Priority + PCP Protocol",
-	/**
-	 * Ditto
-	 */
+        .name = "Priority + PCP Protocol",
+        /**
+         * Ditto
+         */
 };
 
 /***********************************************************************
  * Priority scheduler with priority inheritance protocol
  ***********************************************************************/
 struct scheduler pip_scheduler = {
-	.name = "Priority + PIP Protocol",
-	/**
-	 * Ditto
-	 */
+        .name = "Priority + PIP Protocol",
+        /**
+         * Ditto
+         */
 };
